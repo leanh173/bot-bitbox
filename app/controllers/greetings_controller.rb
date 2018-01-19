587c70123842e2f0cfc9381fb2b1d8d4c17f2be8
leanh173 <leanh173@gmail.com>
2018-01-19 17:18:23 +0700

    add controler exchange selection params

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -2,7 +2,8 @@ class GreetingsController < ApplicationController
   def hello
     list_coin = params[:list_coins] ? params[:list_coins].split(",").map{|a| a.upcase} : nil
     money = params[:money] ? params[:money].to_i : nil
-    bi_box = BiBox.new(money, list_coin)
+    klass_name = params[:exchange] || "Bibox"
+    bi_box = klass_name.constantize.new(money, list_coin)
     bi_box.fetch_good_trade
     @result = bi_box.log
   end
